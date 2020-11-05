0b156fa2dab64f8ddb05c6051ebc89c337525aec
arslanbek <arslonbeksheraliev@gmail.com>
2020-11-05 20:06:27 +0600

    mini calc

--- app/src/main/java/com/example/registrationdesign/MainActivity.java
+++ app/src/main/java/com/example/registrationdesign/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.registrationdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText number1, number2;
+    Button btnPlus, btnMinus;
+    Button btnMulti, btnDivide;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mini_calculator);
+
+        number1 = findViewById(R.id.et_number1);
+        number2 = findViewById(R.id.et_number2);
+
+        btnPlus = findViewById(R.id.btn_plus);
+        btnMinus = findViewById(R.id.btn_minus);
+        btnMulti = findViewById(R.id.btn_multi);
+        btnDivide = findViewById(R.id.btn_divide);
+
+        tvResult = findViewById(R.id.tv_result);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnMulti.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
     }
+
+    @Override
+    public void Onclick(View View) {
+        int res = 0;
+
+        if (view.getId()) == R.id.btn_plus{
+
+            res = Integer.parseInt(number1.getText().toString()) +
+                    Integer.parseInt(number2.getText().toString());
+        }
+        else if (view.getId()) == R.id.btn_minus{
+
+            res = Integer.parseInt(number1.getText().toString()) -
+                    Integer.parseInt(number2.getText().toString());
+    }
+        tvResult.setText("Result: " + res)
 }
